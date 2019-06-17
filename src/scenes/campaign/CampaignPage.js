26c756c5318071306d7627a46103d942c96b8fe9
Jamie Hale <jamiesonhale@gmail.com>
2019-06-17 10:00:00 -0400

    Minor tidy

--- src/scenes/campaign/CampaignPage.js
+++ src/scenes/campaign/CampaignPage.js
@@ -3,7 +3,6 @@ import { compose } from 'redux';
 import { withRouter } from 'react-router';
 import DefaultLayout from '../../layouts/DefaultLayout';
 import useCampaign from '../../hooks/campaign';
-import useCampaignPlaces from '../../hooks/campaign-places';
 import withRemappedRouterParam from '../../components/RemappedRouterParam';
 import CampaignPeople from './CampaignPeople';
 import CampaignPlaces from './CampaignPlaces';
@@ -12,15 +11,12 @@ const CampaignPage = ({
   campaignId,
 }) => {
   const { campaign } = useCampaign(campaignId);
-  const { places, createPlace } = useCampaignPlaces(campaignId);
 
   return (
     <DefaultLayout>
       {campaign && (
-        <h2>{campaign.name}</h2>
-      )}
-      {campaign && (
         <>
+          <h2>{campaign.name}</h2>
           <CampaignPeople campaign={campaign} />
           <CampaignPlaces campaign={campaign} />
         </>
